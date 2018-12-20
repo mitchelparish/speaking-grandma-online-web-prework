4918a0ab9767c24f4282ba6f8e6afbdfd1fb36ee
Mitchel Parish <mitchelparish@gmail.com>
2018-12-20 23:16:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elseif "#{phrase}" == "#{phrase}".downcase
-    return "NO, NOT SINCE 1968"
+    return "HUH?! SPEAK UP, SONNY!"
   else "#{phrase}" == "#{phrase}".upcase
-    return "HUH?! SPEAK UP SONNY!"
+    return "NO, NOT SINCE 1938!"
   end
 end
